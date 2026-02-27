f29bc412d68a772f185b77a94fa45aff5defcf74
alunofatec <alunofatec@lab.fatecsaoroque.edu.br>
2026-02-27 19:21:59 -0300

    atividade 02 - trocar imagem

--- aula02/main.js
+++ aula02/main.js
@@ -1,5 +1,31 @@
 'use strict'
 
-function trocar () {
-    const trocarImagem = document.getElementById(imagem)
+let imagemNova = "img/2k3Splinter.webp"
+let imagemAtual = "img/m.splinter.jpg"
+
+function trocarImagem() {
+    document.getElementById("imagem").src = imagemAtual
+    let guardarImagem = imagemAtual
+    imagemAtual = imagemNova
+    imagemNova = guardarImagem
+}
+
+function salvarTexto() {
+    const texto = document.getElementById("entrada")
+    let lista = document.getElementById("lista")
+    const retorno = document.getElementById("retorno")
+    let salvar = texto.value
+
+    if (salvar != '') {
+        let novaLinha = document.createElement('li')
+        novaLinha.textContent = salvar
+        lista.appendChild(novaLinha)
+        retorno.textContent = "Salvo com sucesso!"
+        texto.value = ''
+    }
+
+    else {
+        retorno.textContent = "Campo vazio, insira sua mensagem!"
+        alert("Mensangem não registrada, campo vazio!")
+    }
 }
